0c3c005b2a4da62b14a8b776d42de63cf8e5a45d
FabriceFormosa <formosafabrice@wanadoo.fr>
2022-09-12 17:56:47 -0400

    remplacer join par resolve

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
